e78bbe00fa03cc9418759c8a38dc51c9fc0d3bd7
dirk.pennings <dirk.pennings@weareyou.com>
2019-12-11 12:16:21 +0100

    Updated Dockerfile to work with Node v10

--- signalr-node-server/app.js
+++ signalr-node-server/app.js
@@ -1,31 +1,39 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const signalR = require('./signalr')(http);
-const port = 3000;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const corsOptions = {
+	origin: 'http://localhost:4567',
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+	credentials: true,
+}
 
-const chat = signalR.mapHub('/chat');
-const clock = signalR.mapHub('/clock');
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions))
+
+const server = require('http').createServer(app)
+const signalR = require('./signalr')(server)
+const port = 3003
+
+const chat = signalR.mapHub('/chatHub')
+const clock = signalR.mapHub('/clock')
 
 setInterval(() => {
-    clock.clients.all.send('tick', Date.now());
-}, 1000);
+	clock.clients.all.send('tick', Date.now())
+}, 1000)
 
-chat.on('connect', (id) => {
-    console.log(`${id} connected`);
-});
+chat.on('connect', id => {
+	console.log(`${id} connected`)
+})
 
-chat.on('disconnect', (id) => {
-    console.log(`${id} disconnected`);
-});
+chat.on('disconnect', id => {
+	console.log(`${id} disconnected`)
+})
 
-chat.on('send', (message) => {
-    chat.clients.all.send('send', message);
-});
+chat.on('SendMessage', (user, message) => {
+	console.log(user, message)
+	chat.clients.all.send('ReceiveMessage', user, message)
+})
 
-app.use(express.static('public'));
-
-http.listen(port, () => {
-    console.log(`listening on *:${port}`);
-});
-
+server.listen(port, () => {
+	console.log(`listening on *:${port}`)
+})
--- signalr-node-server/signalr-http.js
+++ signalr-node-server/signalr-http.js
@@ -1,95 +1,107 @@
-const ws = require('ws');
-const crypto = require('crypto');
-const url = require('url');
+const ws = require('ws')
+const crypto = require('crypto')
+const url = require('url')
 
-const wss = new ws.Server({ noServer: true });
+const wss = new ws.Server({ noServer: true })
 
 // This should be an interface
 class WebSocketConnection {
-    constructor(id, ws) {
-        this.id = id;
-        this._ws = ws;
-    }
+	constructor(id, ws) {
+		this.id = id
+		this._ws = ws
+	}
 
-    onmessage(handler) {
-        this._ws.on('message', handler);
-    }
+	onmessage(handler) {
+		this._ws.on('message', handler)
+	}
 
-    onclose(handler) {
-        this._ws.on('close', handler);
-    }
+	onclose(handler) {
+		this._ws.on('close', handler)
+	}
 
-    send(data) {
-        this._ws.send(data);
-    }
+	send(data) {
+		this._ws.send(data)
+	}
 
-    stop() {
-        this._ws.close();
-    }
+	stop() {
+		this._ws.close()
+	}
 }
 
 class HttpTransport {
-    constructor(basePath, httpServer) {
-        this._basePath = basePath;
-        this._httpServer = httpServer;
-    }
+	constructor(basePath, httpServer) {
+		this._basePath = basePath
+		this._httpServer = httpServer
+	}
 
-    _normalize(path) {
-        if (path[path.length - 1] != '/') {
-            path += '/';
-        }
-        return path;
-    }
+	_normalize(path) {
+		if (path[path.length - 1] != '/') {
+			path += '/'
+		}
+		return path
+	}
 
-    _matches(req, method, parsedUrl, path) {
-        return req.method == method && path === this._normalize(parsedUrl.pathname.substr(0, path.length));
-    }
+	_matches(req, method, parsedUrl, path) {
+		return (
+			req.method == method && path === this._normalize(parsedUrl.pathname.substr(0, path.length))
+		)
+	}
 
-    start(connectionHandler) {
-        var listeners = this._httpServer.listeners('request').slice(0);
-        this._httpServer.removeAllListeners('request');
+	start(connectionHandler) {
+		var listeners = this._httpServer.listeners('request').slice(0)
+		this._httpServer.removeAllListeners('request')
 
-        var path = this._normalize(this._basePath);
-        var negotiatePath = `${path}negotiate/`;
+		var path = this._normalize(this._basePath)
 
-        this._httpServer.on('request', (req, res) => {
-            var parsedUrl = url.parse(req.url, true);
+		console.log(connectionHandler)
+		console.log(path)
 
-            if (this._matches(req, 'POST', parsedUrl, negotiatePath)) {
-                var bytes = crypto.randomBytes(16);
-                var connectionId = bytes.toString('base64');
+		var negotiatePath = `${path}negotiate/`
 
-                res.end(JSON.stringify({
-                    connectionId: connectionId,
-                    availableTransports: [
-                        {
-                            transport: "WebSockets",
-                            transferFormats: ["Text", "Binary"]
-                        }
-                    ]
-                }));
-            }
-            
-            else if (this._matches(req, 'GET', parsedUrl, path)) {
-                if (req.headers["connection"] != "Upgrade") {
-                    res.sendStatus(400);
-                    return;
-                }
+		this._httpServer.on('request', (req, res) => {
+			var parsedUrl = url.parse(req.url, true)
 
-                var id = parsedUrl.query.id;
+			// Set CORS headers
+			res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4567')
+			res.setHeader('Access-Control-Request-Method', '*')
+			res.setHeader('Access-Control-Allow-Methods', '*')
+			res.setHeader('Access-Control-Allow-Headers', 'x-requested-with,content-type')
+			res.setHeader('Access-Control-Allow-Credentials', 'true')
 
-                // Check for upgrade
-                wss.handleUpgrade(req, req.socket, Buffer.alloc(0), (ws) => {
-                    connectionHandler.onConnect(new WebSocketConnection(id, ws));
-                });
-            }
-            else {
-                for (var i = 0, l = listeners.length; i < l; i++) {
-                    listeners[i].call(this._httpServer, req, res);
-                }
-            }
-        });
-    }
+			if (this._matches(req, 'POST', parsedUrl, negotiatePath)) {
+				var bytes = crypto.randomBytes(16)
+				var connectionId = bytes.toString('base64')
+
+				res.end(
+					JSON.stringify({
+						connectionId: connectionId,
+						availableTransports: [
+							{
+								transport: 'WebSockets',
+								transferFormats: ['Text', 'Binary'],
+							},
+						],
+					})
+				)
+			} else if (this._matches(req, 'GET', parsedUrl, path)) {
+				if (req.headers['connection'] != 'Upgrade') {
+					res.sendStatus(400)
+					return
+				}
+
+				var id = parsedUrl.query.id
+
+				// Check for upgrade
+				wss.handleUpgrade(req, req.socket, Buffer.alloc(0), ws => {
+					connectionHandler.onConnect(new WebSocketConnection(id, ws))
+				})
+			} else {
+				for (var i = 0, l = listeners.length; i < l; i++) {
+					listeners[i].call(this._httpServer, req, res)
+				}
+			}
+		})
+	}
 }
 
-module.exports.HttpTransport = HttpTransport;
+module.exports.HttpTransport = HttpTransport
