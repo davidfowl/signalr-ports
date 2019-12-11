7a433dbcfefea68942f6fb2b3c7672535ef886be
dirk.pennings <dirk.pennings@weareyou.com>
2019-12-11 13:01:47 +0100

    updated with prettierrc file

--- signalr-node-server/app.js
+++ signalr-node-server/app.js
@@ -1,39 +1,39 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const corsOptions = {
-	origin: 'http://localhost:4567',
-	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-	credentials: true,
-}
+  origin: 'http://localhost:4567',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true,
+};
 
-app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
-const server = require('http').createServer(app)
-const signalR = require('./signalr')(server)
-const port = 3003
+const server = require('http').createServer(app);
+const signalR = require('./signalr')(server);
+const port = 3003;
 
-const chat = signalR.mapHub('/chatHub')
-const clock = signalR.mapHub('/clock')
+const chat = signalR.mapHub('/chatHub');
+const clock = signalR.mapHub('/clock');
 
 setInterval(() => {
-	clock.clients.all.send('tick', Date.now())
-}, 1000)
+  clock.clients.all.send('tick', Date.now());
+}, 1000);
 
 chat.on('connect', id => {
-	console.log(`${id} connected`)
-})
+  console.log(`${id} connected`);
+});
 
 chat.on('disconnect', id => {
-	console.log(`${id} disconnected`)
-})
+  console.log(`${id} disconnected`);
+});
 
 chat.on('SendMessage', (user, message) => {
-	console.log(user, message)
-	chat.clients.all.send('ReceiveMessage', user, message)
-})
+  console.log(user, message);
+  chat.clients.all.send('ReceiveMessage', user, message);
+});
 
 server.listen(port, () => {
-	console.log(`listening on *:${port}`)
-})
+  console.log(`listening on *:${port}`);
+});
