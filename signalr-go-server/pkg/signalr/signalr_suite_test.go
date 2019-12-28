98c396a087dd839853ec6962fd230762fa8330b5
Philipp Seith <6890503+philippseith@users.noreply.github.com>
2019-12-28 11:09:19 +0100

    new: Tests for streaminvocation_test
    Initial tests
    fixed bug in streamer: Possible race conditions when canceling streaming

--- signalr-go-server/pkg/signalr/signalr_suite_test.go
+++ signalr-go-server/pkg/signalr/signalr_suite_test.go
@@ -5,12 +5,21 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/philippseith/signalr"
 )
 
 func TestSignalr(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Signalr Suite")
 }
+
+func connect(hubProto signalr.HubInterface) *testingHubConnection {
+	server := signalr.NewServer(hubProto)
+	conn := newTestingHubConnection()
+	go server.MessageLoop(conn, "bla", &signalr.JsonHubProtocol{})
+	return conn
+}
+
 
 
 
